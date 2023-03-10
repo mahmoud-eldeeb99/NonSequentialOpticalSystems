74c8faf22513d0ae44b77b84cc41f8907028a2fa
Mahmoud eldeeb <mahmoud.mohammed@eng-st.cu.edu.eg>
2023-03-10 22:12:15 +0200

    Eldeeb

---
+++ class.m
@@ -0,0 +1,22 @@
+classdef class
+   properties
+      Value;
+   end
+   methods
+      
+      
+function r = multiplyBy(obj,val,n)
+         r = val*n;
+end
+
+
+function 
+    
+end    
+
+      
+      
+      
+      
+   end
+end
